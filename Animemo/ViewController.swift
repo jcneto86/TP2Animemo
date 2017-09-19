a90c6ff1f580219f33f4439cb3f6e0cd9ca459d1
João Carlos Fernandes Neto <wysrar@gmail.com>
2017-09-19 11:34:02 -0400

    Initial Commit

---
+++ Animemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animemo
+//
+//  Created by João Carlos Fernandes Neto on 17-09-19.
+//  Copyright © 2017 João Carlos Fernandes Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
