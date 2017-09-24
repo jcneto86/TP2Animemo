ebb68265098523d80fe569df447264d911921a5e
João Carlos Fernandes Neto <wysrar@gmail.com>
2017-09-24 19:55:07 -0400

    v1.2.2

--- Animemo/ViewController.swift
+++ Animemo/ViewController.swift
@@ -1,25 +1,53 @@
-//
-//  ViewController.swift
-//  Animemo
-//
-//  Created by João Carlos Fernandes Neto on 17-09-19.
-//  Copyright © 2017 João Carlos Fernandes Neto. All rights reserved.
-//
-
+//=======================================
 import UIKit
+//=======================================
 
 class ViewController: UIViewController {
+    //=======================================
+    @IBOutlet weak var back1: UIView!
+    @IBOutlet weak var front1: UIView!
+    @IBOutlet weak var back2: UIView!
+    @IBOutlet weak var front2: UIView!
+    @IBOutlet weak var back3: UIView!
+    @IBOutlet weak var front3: UIView!
+    @IBOutlet weak var back4: UIView!
+    @IBOutlet weak var front4: UIView!
+    @IBOutlet weak var back5: UIView!
+    @IBOutlet weak var front5: UIView!
+    @IBOutlet weak var back6: UIView!
+    @IBOutlet weak var front6: UIView!
+    @IBOutlet weak var back7: UIView!
+    @IBOutlet weak var front7: UIView!
+    @IBOutlet weak var back8: UIView!
+    @IBOutlet weak var front8: UIView!
+    @IBOutlet weak var back9: UIView!
+    @IBOutlet weak var front9: UIView!
+    @IBOutlet weak var back10: UIView!
+    @IBOutlet weak var front10: UIView!
+    @IBOutlet weak var back11: UIView!
+    @IBOutlet weak var front11: UIView!
+    @IBOutlet weak var back12: UIView!
+    @IBOutlet weak var front12: UIView!
+    @IBOutlet weak var back13: UIView!
+    @IBOutlet weak var front13: UIView!
+    @IBOutlet weak var back14: UIView!
+    @IBOutlet weak var front14: UIView!
+    @IBOutlet weak var back15: UIView!
+    @IBOutlet weak var front15: UIView!
+    @IBOutlet weak var back16: UIView!
+    @IBOutlet weak var front16: UIView!
+    //=======================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //=======================================
+    @IBAction func showCard(_ sender: Any) {
     }
+    //=======================================
 
-
+    
 }
+//=======================================
 
